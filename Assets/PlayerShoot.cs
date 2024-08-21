8706670c04c6da516974db80d3551582465c88e9
Samuel Dabinett <dabsy004@mymail.unisa.edu.au>
2024-08-21 23:05:19 +0930

    Rework projectile spawning
    The runner now calls the projectile's "Init()" method to set its velocity before it spawns.
    
    Ray is now calculated based on the centre of the viewport rather than the mouse position on the screenport.

--- Assets/PlayerShoot.cs
+++ Assets/PlayerShoot.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class ProjectileShoot : NetworkBehaviour
 {
     public GameObject projectile;
-    public float shootSpeed = 1f;
+    private float shootSpeed = 5f;
 
     public PlayerMovement PlayerMovement;
 
@@ -15,12 +15,20 @@ public class ProjectileShoot : NetworkBehaviour
             return;
         }
 
-        Ray ray = PlayerMovement.Camera.ScreenPointToRay(Input.mousePosition);
+        Ray ray = PlayerMovement.Camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         ray.origin += PlayerMovement.Camera.transform.forward;
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Runner.Spawn(projectile, ray.origin, Quaternion.identity);
+            Runner.Spawn(projectile,
+                ray.origin,
+                Quaternion.identity,
+                Object.InputAuthority,
+                (runner, o) =>
+                {
+                    // Calls the projectile's "Init()" method to set its velocity.
+                    o.GetComponent<PhysxBall>().Init(PlayerMovement.Camera.transform.forward * shootSpeed);
+                });
         }
     }
 
