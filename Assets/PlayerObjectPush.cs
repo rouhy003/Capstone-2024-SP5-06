391bbd9004705f825b3e264096a97997c86a7141
Samuel Dabinett <dabsy004@mymail.unisa.edu.au>
2024-08-21 23:07:18 +0930

    Add cardboard box and push functionality
    Cardboard boxes can be pushed by both the spawned projectiles and the players themselves.

---
+++ Assets/PlayerObjectPush.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Fusion;
+
+public class PlayerObjectPush : NetworkBehaviour
+{
+    public float pushPower = 2f;
+
+    void OnControllerColliderHit(UnityEngine.ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+
+        if (hit.moveDirection.y < -0.3f)
+        {
+            return;
+        }
+
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        body.velocity = pushDir * pushPower;
+    }
+}
