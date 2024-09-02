765103d17b3209951db1864487dee817427b8eb6
Samuel Dabinett <dabsy004@mymail.unisa.edu.au>
2024-09-02 23:50:42 +0930

    Add partial object spawning logic
    Currently doesn't function because of an error regarding object instance. Will need to fix.

--- Assets/Objects/ObjectManager.cs
+++ Assets/Objects/ObjectManager.cs
@@ -1,20 +1,33 @@
 using Fusion;
-using Oculus.Interaction;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Pool;
-using UnityEngine.UIElements;
+using System.Collections.Generic;
 
 public class ObjectManager : NetworkBehaviour
 {
-    [SerializeField] private GameObject objectPrefab;
+    public GameObject objectPrefab;
 
-    [SerializeField] private List<NetworkObject> objectPool = new List<NetworkObject>();
-    [Networked] private int CurrentObjects { get; set; }
-    [SerializeField] private const int ObjectLimit = 3;
+    [SerializeField] private List<NetworkObject> currentObjects { get; set; }
+    public const int ObjectLimit = 5;
 
     public override void FixedUpdateNetwork()
     {
         // TODO: Insert spawning functionality here.
+        if (currentObjects.Count < ObjectLimit)
+        {
+            SpawnObject(Random.Range(10, -10), Random.Range(10, -10));
+        }
+        Debug.Log(currentObjects.Count);
+    }
+    public void SpawnObject(float x, float z)
+    {
+        Vector3 spawnPosition = new Vector3(x, 5f, z);
+        NetworkObject spawnedObject = Runner.Spawn(objectPrefab, spawnPosition);
+        if (spawnedObject != null) currentObjects.Add(spawnedObject);
+    }
+
+    //[Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public void DespawnObject(NetworkObject spawnedObject)
+    {
+        Runner.Despawn(spawnedObject);
     }
 }
