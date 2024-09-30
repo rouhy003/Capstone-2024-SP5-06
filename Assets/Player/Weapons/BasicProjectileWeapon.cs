dd6e8bdeb89307df601867c9a6cd1f2af6af0e95
Harrison Rouse <rouhy003@mymail.unisa.edu.au>
2024-09-30 14:34:15 +0930

    Fixed nullReferenceException for basicProjectileWeapon

--- Assets/Player/Weapons/BasicProjectileWeapon.cs
+++ Assets/Player/Weapons/BasicProjectileWeapon.cs
@@ -15,7 +15,10 @@ public class BasicProjectileWeapon : GenericWeapon
 
     public override void Shoot()
     {
-        NetworkObject proj = Runner.Spawn(projectile, FirePoint.position);
-        proj.GetComponent<PhysxBall>().Fire(FirePoint.position, GetFiringRayOrigin() * shootSpeed);
+        if (Runner != null)
+        {
+            NetworkObject proj = Runner.Spawn(projectile, FirePoint.position);
+            proj.GetComponent<PhysxBall>().Fire(FirePoint.position, GetFiringRayOrigin() * shootSpeed);
+        }
     }
 }
