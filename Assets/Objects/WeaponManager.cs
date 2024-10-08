d554d7f9f400f98821538f026835d6b09c5e79c0
Samuel Dabinett <dabsy004@mymail.unisa.edu.au>
2024-10-09 00:01:23 +1030

    Remove duplicate attributes from WeaponManager

--- Assets/Objects/WeaponManager.cs
+++ Assets/Objects/WeaponManager.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class WeaponManager : NetworkBehaviour
 {
-    [SerializeField]
-    public GameObject[] weaponPrefabs;
-
-    // Spawning boundaries for each weapon
-    private Vector3 spawnBoundaries = new Vector3(0.5f, 0.2f, 0.5f);
-
-    private List<NetworkObject> currentWeapons = new List<NetworkObject>();
-
-    public const int WeaponLimit = 3;
+    
 }
