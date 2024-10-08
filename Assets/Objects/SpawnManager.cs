1a55b80dd637ab80017da08d8f310d6e41d03004
Samuel Dabinett <dabsy004@mymail.unisa.edu.au>
2024-10-08 23:49:49 +1030

    Import spawning functionality from ObjectManager

--- Assets/Objects/SpawnManager.cs
+++ Assets/Objects/SpawnManager.cs
@@ -30,7 +30,59 @@ public class SpawnManager : NetworkBehaviour
     public bool SpawnObject(GameObject prefab, Vector3 spawnPosition)
     {
         bool canSpawn = false;
-        return canSpawn;
+
+        SpawnableObject prop = prefab.GetComponent<SpawnableObject>();
+        if (prop != null)
+        {
+            // Grounded object spawning
+            if (prop.spawnsOnGround())
+            {
+                RaycastHit groundCast;
+                bool hasGround = Physics.Linecast(spawnPosition, spawnPosition + (10 * Vector3.down), out groundCast);
+
+                if (hasGround && groundCast.collider.tag == "Ground")
+                {
+                    spawnPosition.Set(spawnPosition.x, groundCast.point.y + (spawnBoundaries.y / 2), spawnPosition.z);
+
+                    // Checks to make sure that there is enough space to spawn the object
+                    Collider[] overlap = Physics.OverlapBox(spawnPosition, new Vector3(spawnBoundaries.x, spawnBoundaries.y * 0.49f, spawnBoundaries.z));
+                    if (overlap.Length < 1)
+                    {
+                        canSpawn = true;
+                    }
+                }
+            }
+            else
+            {
+                Collider[] overlap = Physics.OverlapSphere(spawnPosition, 0.5f);
+                if (overlap.Length < 1)
+                {
+                    canSpawn = true;
+                }
+            }
+        }
+
+        // Checking to make sure that the object is within the perimeters of the room.
+        Vector3[] directions = { Vector3.forward, Vector3.left, Vector3.right, Vector3.back };
+        foreach (Vector3 vector in directions)
+        {
+            RaycastHit wallCast;
+            LayerMask mask = LayerMask.GetMask("Wall");
+
+            // If a wall isn't detected in at least one of the rays, then the object will not spawn.s
+            if (!Physics.Raycast(spawnPosition, spawnPosition + vector , out wallCast, 20f, mask))
+            {
+                canSpawn = false;
+            }
+        }
+
+        if (canSpawn)
+        {
+            NetworkObject spawnedObject = Runner.Spawn(prefab, spawnPosition);
+            currentObjects.Add(spawnedObject);
+            return true;
+        }
+        return false;
     }
 
     // Returns a random spawn position.
