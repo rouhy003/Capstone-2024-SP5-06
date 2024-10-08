9389ad7010a83d589981fbca2692523df3b43a65
Samuel Dabinett <dabsy004@mymail.unisa.edu.au>
2024-10-09 00:00:05 +1030

    Rename "gm" to "gameManager"

--- Assets/Objects/SpawnManager.cs
+++ Assets/Objects/SpawnManager.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 
 public class SpawnManager : NetworkBehaviour
 {
+    // Represents the current scene's game manager (if it exists).
+    protected GameManager gameManager;
+
     [SerializeField]
     public GameObject[] spawnPrefabs;
 
     // Represent's the object's size to calculate spawning space
     [SerializeField]
     protected Vector3 spawnBoundaries = new Vector3(0.3f, 0.5f, 0.3f);
-
-    protected GameManager gm;
 
     protected List<NetworkObject> currentObjects = new List<NetworkObject>();
 
@@ -23,7 +24,7 @@ public class SpawnManager : NetworkBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gm = FindObjectOfType<GameManager>();
+        gameManager = FindObjectOfType<GameManager>();
     }
 
     // Attempts to spawn the specified prefab at the specified spawn position.
