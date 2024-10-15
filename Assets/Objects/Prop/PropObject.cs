a3d7fb78663b10ebf5fe152b59acdfea320c44ac
Samuel Dabinett <dabsy004@mymail.unisa.edu.au>
2024-10-15 16:38:56 +1030

    Add hit sounds to objects

--- Assets/Objects/Prop/PropObject.cs
+++ Assets/Objects/Prop/PropObject.cs
@@ -4,12 +4,12 @@ using System.Collections.Generic;
 
 public class PropObject : NetworkBehaviour
 {
-    private bool hasBeenStruck = false;
+    [SerializeField] private AudioSource hitSound;
 
     [Networked] TickTimer life { get; set; }
-
     [SerializeField] private float objectLifetime = 3;
     [SerializeField] private float knockdownTime = 0;
+    private bool hasBeenStruck = false;
 
     [SerializeField] private int pointValue = 1;
     [SerializeField] private bool addPoints = true;
@@ -33,9 +33,10 @@ public class PropObject : NetworkBehaviour
     // Executes whenever the object is struck by a projectile.
     public void Knockdown(int player)
     {
-        // Resets the object's life if not previously struck before.
+        // Can only be ran through once per object
         if (!hasBeenStruck)
         {
+            // Adds points to owner of the projectile who knocked down the object.
             if (player == 1)
             {
                 sm.ChangeP1ScoreRPC(addPoints, pointValue);
@@ -44,7 +45,14 @@ public class PropObject : NetworkBehaviour
             {
                 sm.ChangeP2ScoreRPC(addPoints, pointValue);
             }
+
+            hitSound.Play();
+
+            // Sets a timer for the object's remaining lifetime.
+            // Have "knockdownTime" set to 0 to have objects despawn immediately upon impact.
             life = TickTimer.CreateFromSeconds(Runner, knockdownTime);
+
+            hasBeenStruck = true;
         }
     }
 
