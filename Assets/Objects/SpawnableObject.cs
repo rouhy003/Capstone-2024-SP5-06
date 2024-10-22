2103d867047d96ed14a0c0f2edc67868a5be7da6
Samuel Dabinett <dabsy004@mymail.unisa.edu.au>
2024-10-22 17:27:37 +1030

    Update SpawnableObject to reference SpawnManager
    Also added a Despawn() method directly to the script, which directly calls the despawn method from the referenced manager.

--- Assets/Objects/SpawnableObject.cs
+++ Assets/Objects/SpawnableObject.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 
 public class SpawnableObject : NetworkBehaviour
 {
+    private SpawnManager spawnManager;
+
+    // Determines whether the object will spawn anchored to the ground.
     [SerializeField] private bool spawnsGrounded;
 
     // Represents the spawning chance of the object
     // Should be set from 0 to 1, where 1 means that the object will always spawn on every attempt.
     [SerializeField] private float spawnChance;
 
+    // Represents the volume of space that needs to be empty for the object to spawn.
     [SerializeField] private Vector3 spawnBoundaries;
+    
+    public void SetSpawnManager(SpawnManager setManager) { spawnManager = setManager; }
 
-    public Vector3 getSpawnBoundaries()
-    {
-        return spawnBoundaries;
-    }
-    public bool spawnsOnGround()
-    {
-        return spawnsGrounded;
-    }
-    public float getSpawnChance()
-    {
-        return spawnChance;
-    }
+    public Vector3 getSpawnBoundaries() { return spawnBoundaries; }
+    public bool spawnsOnGround() { return spawnsGrounded; }
+    public float getSpawnChance() { return spawnChance; }
+
+    // Calls the referenced spawn manager to despawn this current object.
+    public void Despawn() { spawnManager.DespawnObject(Object); }
 }
